15dcfee0d762dc08923bdd93641e725751a3847e
masatsuna18 <62019850+masatsuna18@users.noreply.github.com>
2024-04-20 16:09:08 +0900

    Update import.js

--- mw-to-cf7/import.js
+++ mw-to-cf7/import.js
@@ -1,1 +1,28 @@
+function MwToCf7Import() {
+    // ファイル選択ウィンドウを開く
+    let inputElement = document.createElement('input');
+    inputElement.type = "file";
+    inputElement.accept = ".json"; // デフォルトの形式はjsonファイル
+    inputElement.click();
+
+    inputElement.onchange = function(event) {
+        let fileReader = new FileReader();
+        fileReader.onload = function() {
+            let data = JSON.parse(this.result);
+
+            // "convertedCode"の値を"textarea[name='wpcf7-form']"に挿入
+            let textareaElement = document.querySelector('textarea[name="wpcf7-form"]');
+            if (textareaElement && data.convertedCode) {
+                textareaElement.value = data.convertedCode;
+            }
+
+            // "mailToValue"の値を"input[name='wpcf7-mail[recipient]']"に挿入
+            let inputElement = document.querySelector('input[name="wpcf7-mail[recipient]"]');
+            if (inputElement && data.mailToValue) {
+                inputElement.value = data.mailToValue;
+            }
+        };
+        fileReader.readAsText(event.target.files[0]);
+    };
+}
 
