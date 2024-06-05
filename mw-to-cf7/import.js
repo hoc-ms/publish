0087a096c35166957ae694b7a2f4dc4952893a7f
masatsuna18 <62019850+masatsuna18@users.noreply.github.com>
2024-06-05 09:50:30 +0900

    Update import.js

--- mw-to-cf7/import.js
+++ mw-to-cf7/import.js
@@ -1,70 +1,82 @@
 function MwToCf7Import() {
-  // ファイル選択ウィンドウを開く
-  let fileInput = document.createElement('input');
-  fileInput.type = 'file';
-  fileInput.accept = '.json'; // デフォルトの形式はjsonファイル
-  fileInput.click();
+    // ファイル選択ウィンドウを作成
+    let fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = '.json';
+    fileInput.click();
 
-  fileInput.onchange = function(event) {
-      let fileReader = new FileReader();
-      fileReader.onload = function() {
-          let data = JSON.parse(this.result);
+    fileInput.onchange = function (event) {
+        let fileReader = new FileReader();
+        fileReader.onload = function () {
+            // ファイルの内容をJSONとして解析
+            let data = JSON.parse(this.result);
 
-          // データの各要素をループ処理
-          for (let i = 0; i < data.length; i++) {
-              let item = data[i];
+            // データをオブジェクトに変換
+            let item = Object.assign({}, ...data);
 
-              // 'postTitle'の値を'input[name="post_title"]'に挿入
-              if (item.formContent && item.formContent.postTitle) {
-                  let postTitleInput = document.querySelector('input[name="post_title"]');
-                  if (postTitleInput) {
-                      postTitleInput.value = item.formContent.postTitle;
-                  }
-              }
+            // フィールドとセレクタのマッピングを定義
+            const fields = {
+                'formContent.postTitle': 'input[name="post_title"]',
+                'formContent.convertedCode': 'textarea[name="wpcf7-form"]',
+                'adminMail.mailToValue': 'input[name="wpcf7-mail[recipient]"]',
+                'adminMail.adminMailSubject': 'input[name="wpcf7-mail[subject]"]',
+                'adminMail.adminMailContent': 'textarea[name="wpcf7-mail[body]"]',
+                'mail.automaticReplyEmail': 'input[name="wpcf7-mail-2[recipient]"]',
+                'mail.mailSubject': 'input[name="wpcf7-mail-2[subject]"]',
+                'mail.mailContent': 'textarea[name="wpcf7-mail-2[body]"]',
+            };
 
-              // 'convertedCode'の値を'textarea[name="wpcf7-form"]'に挿入
-              if (item.formContent && item.formContent.convertedCode) {
-                  let formTextarea = document.querySelector('textarea[name="wpcf7-form"]');
-                  if (formTextarea) {
-                      formTextarea.value = item.formContent.convertedCode;
-                  }
-              }
+            // 各フィールドに対して処理を行う
+            Object.keys(fields).forEach(field => {
+                // フィールド名をドットで分割し、各部分を取得
+                let keys = field.split('.');
+                let value = item;
+                // 各キーに対して値を取得
+                keys.forEach(key => {
+                    value = value ? value[key] : null;
+                });
+                // 値が存在する場合、対応する要素に値を設定
+                if (value) {
+                    let input = document.querySelector(fields[field]);
+                    if (input) {
+                        input.value = value;
+                    }
+                }
+            });
 
-              // 'mailToValue'の値を'input[name="wpcf7-mail[recipient]"]'に挿入
-              if (item.adminMail && item.adminMail.mailToValue) {
-                  let mailToValueInput = document.querySelector('input[name="wpcf7-mail[recipient]"]');
-                  if (mailToValueInput) {
-                      mailToValueInput.value = item.adminMail.mailToValue;
-                  }
-              }
+            // 添付ファイルを反映
+            let filePatternMatches = item.formContent.convertedCode.match(/\[file\s([^\s]+)[^\]]*\]/g);
+            if (filePatternMatches) {
+                let fileNamesArray = filePatternMatches.map(match => match.split(' ')[1]);
+                let concatenatedFileNames = fileNamesArray.map(fileName => `[${fileName}]`).join(' ');
+                document.querySelector('textarea[name="wpcf7-mail[attachments]"]').value = concatenatedFileNames;
+            }
 
-              // 'adminMailSubject'の値を'input[name="wpcf7-mail[subject]"]'に挿入
-              if (item.adminMail && item.adminMail.adminMailSubject) {
-                  let adminMailSubjectInput = document.querySelector('input[name="wpcf7-mail[subject]"]');
-                  if (adminMailSubjectInput) {
-                      adminMailSubjectInput.value = item.adminMail.adminMailSubject;
-                  }
-              }
+            // メール(2)を有効化
+            let activeCheckbox = document.querySelector('input[name="wpcf7-mail-2[active]"]');
+            if (activeCheckbox && !activeCheckbox.checked) {
+                activeCheckbox.click();
+            }
 
-              // 'adminMailContent'の値を'textarea[name="wpcf7-mail[body]"]'に挿入
-              if (item.adminMail && item.adminMail.adminMailContent) {
-                  let adminMailContentTextarea = document.querySelector('textarea[name="wpcf7-mail[body]"]');
-                  if (adminMailContentTextarea) {
-                      adminMailContentTextarea.value = item.adminMail.adminMailContent;
-                  }
-              }
+            // 編集が必要な文字列を処理
+            if (item.adminMail) {
+                let adminMailSender = document.querySelector('input[name="wpcf7-mail[sender]"]');
+                if (adminMailSender) {
+                    adminMailSender.value = item.adminMail.adminMailSender + ' <' + item.adminMail.adminMailFrom + '>';
+                }
+            }
 
-              // 'mailSubject'の値を'input[name="wpcf7-mail-2[recipient]"]'に挿入
-              if (item.adminMail && item.adminMail.mailSubject) {
-                  let mailSubjectInput = document.querySelector('input[name="wpcf7-mail-2[recipient]"]');
-                  if (mailSubjectInput) {
-                      mailSubjectInput.value = item.adminMail.mailSubject;
-                  }
-              }
-
-          }
-      };
-      fileReader.readAsText(event.target.files[0]);
-  };
+            if (item.mail) {
+                let mailSender = document.querySelector('input[name="wpcf7-mail-2[sender]"]');
+                if (mailSender) {
+                    mailSender.value = item.mail.mailSender + ' <' + item.mail.mailFrom + '>';
+                }
+            }
+        };
+        // ファイルの内容をテキストとして読み込む
+        fileReader.readAsText(event.target.files[0]);
+    };
 }
+
+// 関数を実行
 MwToCf7Import();
