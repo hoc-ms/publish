f36a26f7a6c1d9fc541ca55cecd0630bc7e373ac
masatsuna18 <62019850+masatsuna18@users.noreply.github.com>
2024-06-06 13:52:38 +0900

    Update import.js

--- mw-to-cf7/import.js
+++ mw-to-cf7/import.js
@@ -16,11 +16,14 @@ function MwToCf7Import() {
 
             // フィールドとセレクタのマッピングを定義
             const fields = {
+                // フォーム
                 'formContent.postTitle': 'input[name="post_title"]',
                 'formContent.convertedCode': 'textarea[name="wpcf7-form"]',
+                // メール(1)
                 'adminMail.mailToValue': 'input[name="wpcf7-mail[recipient]"]',
                 'adminMail.adminMailSubject': 'input[name="wpcf7-mail[subject]"]',
                 'adminMail.adminMailContent': 'textarea[name="wpcf7-mail[body]"]',
+                // メール(2)
                 'mail.automaticReplyEmail': 'input[name="wpcf7-mail-2[recipient]"]',
                 'mail.mailSubject': 'input[name="wpcf7-mail-2[subject]"]',
                 'mail.mailContent': 'textarea[name="wpcf7-mail-2[body]"]',
@@ -53,23 +56,51 @@ function MwToCf7Import() {
             }
 
             // メール(2)を有効化
-            let activeCheckbox = document.querySelector('input[name="wpcf7-mail-2[active]"]');
-            if (activeCheckbox && !activeCheckbox.checked) {
-                activeCheckbox.click();
+            let mail2Checkbox = document.querySelector('input[name="wpcf7-mail-2[active]"]');
+            if (mail2Checkbox && !mail2Checkbox.checked) {
+                mail2Checkbox.click();
             }
 
-            // 編集が必要な文字列を処理
+            // 編集が必要な文字列を処理してから入力
+            // メール(1)
             if (item.adminMail) {
-                let adminMailSender = document.querySelector('input[name="wpcf7-mail[sender]"]');
-                if (adminMailSender) {
-                    adminMailSender.value = item.adminMail.adminMailSender + ' <' + item.adminMail.adminMailFrom + '>';
+                // 送信元
+                let mail1SenderInput = document.querySelector('input[name="wpcf7-mail[sender]"]');
+                if (mail1SenderInput) {
+                    let adminMailSender = item.adminMail.adminMailSender || '[_site_title]';
+                    let adminMailFrom = item.adminMail.adminMailFrom || 'contact@' + window.location.hostname;
+                    mail1SenderInput.value = `${adminMailSender} <${adminMailFrom}>`;
+                }
+                // 追加ヘッダー
+                let mail1AdditionalHeaders = document.querySelector('textarea[name="wpcf7-mail[additional_headers]"]');
+                if (mail1AdditionalHeaders) {
+                    let adminMailReplyTo = item.adminMail.adminMailReplyTo || item.mail.automaticReplyEmail;
+                    let mailCcValue = item.adminMail.mailCcValue || "";
+                    let mailBccValue = item.adminMail.mailBccValue || "";
+                    mail1AdditionalHeaders.value = adminMailReplyTo ? `Reply-To: ${adminMailReplyTo}` : '';
+                    if (mailCcValue) {
+                        mail1AdditionalHeaders.value += `\nCc: ${mailCcValue}`;
+                    }
+                    if (mailBccValue) {
+                        mail1AdditionalHeaders.value += `\nBcc: ${mailBccValue}`;
+                    }
                 }
             }
 
+            // メール(2)
             if (item.mail) {
-                let mailSender = document.querySelector('input[name="wpcf7-mail-2[sender]"]');
-                if (mailSender) {
-                    mailSender.value = item.mail.mailSender + ' <' + item.mail.mailFrom + '>';
+                // 送信元
+                let mail2SenderInput = document.querySelector('input[name="wpcf7-mail-2[sender]"]');
+                if (mail2SenderInput) {
+                    let senderName = item.mail.mailSender || '[_site_title]';
+                    let senderEmail = item.mail.mailFrom || 'contact@' + window.location.hostname;
+                    mail2SenderInput.value = `${senderName} <${senderEmail}>`;
+                }
+                // 追加ヘッダー
+                let mail2AdditionalHeaders = document.querySelector('textarea[name="wpcf7-mail-2[additional_headers]"]');
+                if (mail2AdditionalHeaders) {
+                    let mailReplyTo = item.mail.mailReplyTo || "";
+                    mail2AdditionalHeaders.value = mailReplyTo ? `Reply-To: ${mailReplyTo}` : '';
                 }
             }
         };
